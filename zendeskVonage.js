60c22b27b9e2505b54ad1a74526747245db37f5f
Toni Kuschan <toni.kuschan@plevendo.com>
2022-03-14 17:04:26 +0000

    more updates

--- zendeskVonage.js
+++ zendeskVonage.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const Vonage = require('@vonage/server-sdk')
 var express = require('express')
 const https = require('https')
+const localtunnel = require('localtunnel')
 
 const PORT = process.env.PORT || 3000
 const DEBUG = process.env.DEBUG || false
@@ -13,43 +14,44 @@ const vonage = new Vonage({
   privateKey: process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH,
 }, { debug: DEBUG })
 
-const VONAGE_WHATSAPP_NUMBER = process.env.VONAGE_WHATSAPP_NUMBER;
+const VONAGE_WHATSAPP_NUMBER = process.env.VONAGE_WHATSAPP_NUMBER
+const VONAGE_WHATSAPP_NAMESPACE = process.env.VONAGE_WHATSAPP_NAMESPACE
+const VONAGE_WHATSAPP_TICKET_TEMPLATE_NAME = process.env.VONAGE_WHATSAPP_TICKET_TEMPLATE_NAME
 
-const ZENDESK_CREDENTIALS = process.env.ZENDESK_CREDENTIALS;
-const ZENDESK_DOMAIN = process.env.ZENDESK_DOMAIN;
+const ZENDESK_CREDENTIALS = process.env.ZENDESK_CREDENTIALS
+const ZENDESK_SUBDOMAIN = process.env.ZENDESK_SUBDOMAIN
+const ZENDESK_DEFAULT_PRIORITY = process.env.ZENDESK_DEFAULT_PRIORITY || "urgent"
 
 const app = express()
 app.use(express.json())
 
 //Inbound messages webhook added in Vonage nexmo dashbaord creates a ticekt in Zendesk
 app.post('/inboundMessage', (req, res) => {
-  var ticket_title = req.body.text;
-  var toNumber = req.body.from;
-  createZendeskTicket(ticket_title, toNumber);
+  const { profile, text, from } = req.body
+  const requesterName = profile.name
+  createZendeskTicket(text, from, requesterName);
   res.json(200);
 })
 
-
 //Send outbound message using Vonage Vonage APIs
-function sendMessage(requesterNumber, message) {
-
+function sendMessage(zendeskRequesterNumber, message, isTemplateMessage) {
   //channel could be set to 'sms' or 'whatsapp'
-  var channel = "whatsapp";
+  const channel = "whatsapp";
+
+  // check if message is template or text and prepare accordingly
+  const sendableMessage = isTemplateMessage ? { message_type: "custom", custom: message } : { message_type: "text", text: message }
 
   const data = JSON.stringify({
-    "from": VONAGE_WHATSAPP_NUMBER,
-    "to": requesterNumber,
-    "message_type": "text",
-    "text": message,
-    "channel": channel
+    from: VONAGE_WHATSAPP_NUMBER,
+    to: zendeskRequesterNumber,
+    channel,
+    ...sendableMessage
   })
 
+  // use vonage sdk to generate JWT token
   const JWT = vonage.credentials.generateJwt()
 
   const options = {
-    hostname: 'api.nexmo.com',
-    port: 443,
-    path: '/v1/messages',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -57,12 +59,8 @@ function sendMessage(requesterNumber, message) {
     }
   }
 
-  const req = https.request(options, res => {
+  const req = https.request("https://api.nexmo.com/v1/messages", options, res => {
     console.log(`statusCode: ${res.statusCode}`)
-
-    res.on('data', d => {
-      process.stdout.write(d)
-    })
   })
 
   req.on('error', error => {
@@ -74,49 +72,73 @@ function sendMessage(requesterNumber, message) {
   req.end()
 }
 
-//Zendesk ticket update webhook sends an outbound message back to the user
+// Zendesk ticket update webhook sends an outbound message back to the user
 app.post('/getTicketUpdate', (req, res) => {
-  var requesterNumber = req.body.requester_name;
-  var zendeskComment = req.body.comment;
-  var message = "Ticket " + req.body.ticket_title + " with reference number: " + req.body.ticket_id +
-    " has been updated with the following comment: " + zendeskComment;
-  sendMessage(requesterNumber, message);
+  const { requesterPhone: zendeskRequesterPhone, requesterName: zendeskRequesterName, comment: zendeskComment, ticketId: zendeskTicketId, ticketTitle: zendeskTicketTitle } = req.body
+  const isTemplateMessage = (VONAGE_WHATSAPP_NAMESPACE && VONAGE_WHATSAPP_TICKET_TEMPLATE_NAME && !zendeskComment.includes("\n")) ? true : false
+  // if template data is filled, use templates to send; otherwise use plain text message
+  const message = isTemplateMessage ? {
+    type: "template",
+    template: {
+      namespace: VONAGE_WHATSAPP_NAMESPACE,
+      name: VONAGE_WHATSAPP_TICKET_TEMPLATE_NAME,
+      language: {
+        policy: "deterministic",
+        code: "en"
+      },
+      components: [
+        {
+          type: "header",
+          parameters: [
+            {
+              type: "text",
+              text: `Zendesk Ticket #${zendeskTicketId}`
+            }
+          ]
+        },
+        {
+          type: "body",
+          parameters: [
+            {
+              type: "text",
+              text: `${zendeskComment.replaceAll("\n", " ")}`
+            },
+          ]
+        },
+      ]
+    }
+  } : `Your Zendesk Ticket *${zendeskTicketTitle}* with reference number *${zendeskTicketId}* has been updated with the following comment:\n\n${zendeskComment}`;
+  sendMessage(zendeskRequesterPhone, message, isTemplateMessage);
   res.json(200);
 });
 
 
-function createZendeskTicket(ticket_title, toNumber) {
-  //below object ticket is only for demo purpose
+function createZendeskTicket(text, from, name) {
+  // below object ticket is only for demo purpose
   const data = JSON.stringify({
-    "ticket": {
-      "comment": {
-        "body": "The smoke is very colorful."
+    ticket: {
+      comment: {
+        body: `${text}`
       },
-      "priority": "urgent",
-      "subject": ticket_title,
-      "requester": {
-        "name": toNumber
+      priority: ZENDESK_DEFAULT_PRIORITY,
+      subject: `Whatsapp request: ${text.substr(0, 20)}...`,
+      requester: {
+        name,
+        phone: from
       }
     }
   })
 
   const options = {
-    hostname: ZENDESK_DOMAIN,
-    port: 443,
-    path: '/api/v2/tickets.json',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Basic ' + ZENDESK_CREDENTIALS
+      'Authorization': 'Basic ' + Buffer.from(ZENDESK_CREDENTIALS).toString("base64")
     }
   }
 
-  const req = https.request(options, res => {
+  const req = https.request(`https://${ZENDESK_SUBDOMAIN}.zendesk.com/api/v2/tickets.json`, options, res => {
     console.log(`statusCode: ${res.statusCode}`)
-
-    res.on('data', d => {
-      process.stdout.write(d)
-    })
   })
 
   req.on('error', error => {
@@ -128,4 +150,29 @@ function createZendeskTicket(ticket_title, toNumber) {
   req.end()
 }
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+let tunnel;
+app.listen(PORT, () => {
+  console.log(`Hello world app listening on port ${PORT}! Starting localtunnel...`)
+  tunnel = localtunnel(PORT, { subdomain: 'zendesk' }, (err, tunnel) => {
+    console.log(`Established localtunnel at ${tunnel.url}.`)
+  })
+  tunnel.on('error', function (err) {
+    // When the tunnel is erroring out
+    console.error(err)
+  })
+  tunnel.on('close', function () {
+    // When the tunnel is closed
+    console.warn("Localtunnel closed.")
+  })
+})
+
+app.on('close', () => {
+  console.log("Closing localtunnel...")
+  tunnel.close()
+})
+
+process.on('SIGINT', function () {
+  console.log("Closing localtunnel...")
+  tunnel.close()
+  process.exit()
+});
